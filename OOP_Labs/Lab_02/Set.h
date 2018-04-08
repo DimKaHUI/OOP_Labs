41a3c18be69ead00461e746958cd2d2d92760efb
DimKaHUI <wet75787@tqosi.com>
2018-04-08 17:38:06 +0300

    Implemented

--- OOP_Labs/Lab_02/Set.h
+++ OOP_Labs/Lab_02/Set.h
@@ -1,6 +1,9 @@
 #pragma once
 
-namespace Lab_02
+#include <iostream>
+#include <string>
+
+namespace Util
 {
 	template<typename T>
 	class Set
@@ -9,37 +12,220 @@ namespace Lab_02
 		T *data_;
 		int length_;
 	public:
-		Set(T elements[], int count);
-		Set(const Set& obj);
-		Set(Set&& obj);
-		Set();
-		~Set();
+		Set(const T *elements, int count)
+		{
+			int actualLength = count;
+			T *temp = new T[count];
+			int tempIndex = 0;
+			for (int i = 0; i < count; i++)
+			{
+				bool exists = false;
+				for (int j = 0; j < tempIndex; j++)
+				if (temp[j] == elements[i])
+				{
+					exists = true;
+					break;
+				}
+				if (!exists)
+				{
+					temp[tempIndex] = elements[i];
+					tempIndex++;
+				}
+				else
+					actualLength--;
+			}
+
+			data_ = new T[actualLength];
+			for (int i = 0; i < actualLength; i++)
+			{
+				data_[i] = temp[i];
+			}
+			length_ = actualLength;
+
+			delete[] temp;
+		}
+
+		Set(const Set<T>& obj)
+		{
+			this->length_ = obj.length_;
+			this->data_ = new T[obj.length_];
+			for (int i = 0; i < this->length_; i++)
+				this->data_[i] = obj.data_[i];
+		}
+
+		/*Set(Set&& obj)
+		{
+			data_ = obj.data_;
+			length_ = obj.length_;
+		}*/
+		
+		Set()
+		{
+			length_ = 0;
+			data_ = nullptr;
+		}
+
+		~Set()
+		{
+			delete[] data_;
+		}
 
 		// Сравнение множеств
-		bool operator==(const Set& other) const;
+		bool operator==(const Set& other) const
+		{
+			if (length_ != other.length_)
+			{
+				return false;
+			}
+			for (int i = 0; i < length_; i++)
+			if (!contains(other.data_[i]))
+			{
+				return false;
+			}
+			return true;
+		}
 
 		// Объединение множеств
-		void operator+=(const Set& other);
+		void operator+=(const Set& other)
+		{
+			for (int i = 0; i < other.length_; i++)
+				this->add(other.data_[i]);
+		}
 
-		Set& operator +(const Set& b);
-		Set& operator *(const Set& b);
-		Set& operator -(const Set& b);
+		void operator+=(const T element)
+		{
+			add(element);
+		}	
+
+		Set operator +(const Set& b)
+		{
+			Set<T> united = Set<T>(*this);
+			united += b;
+			return united;
+		}
+
+		Set operator *(const Set& b)
+		{
+			Set<T> res = Set(*this);
+			res *= b;
+			return res;
+		}
+
+		Set operator -(const Set& b)
+		{
+			Set<T> res = Set(*this);
+			res -= b;
+			return res;
+		}
 
 		// Разность множеств
-		void operator-=(const Set& other);
+		void operator-=(const Set& other)
+		{
+			for (int i = 0; i < length_; i++)
+			if (other.contains(data_[i]))
+			{
+				remove(data_[i]);
+				i--;
+			}
+		}
+
+		void operator-=(const T element)
+		{
+			remove(element);
+		}
 
 		// Пересечение множеств
-		void operator*=(const Set& other);
+		void operator*=(const Set& other)
+		{
+			for (int i = 0; i < length_; i++)
+			if (!other.contains(data_[i]))
+			{
+				remove(data_[i]);
+				i--;
+			}			
+		}
 
 		// Получение текущего размера
-		int get_length() const;
+		int get_length() const
+		{
+			return length_;
+		}
 
-		void add(T element);
+		void add(const T element)
+		{
+			if (contains(element))
+				return;
 
-		void remove(T element);
+			T *tmp = new T[length_ + 1];
+			
+			for (int i = 0; i < length_; i++)
+				tmp[i] = data_[i];
+			tmp[length_] = element;
 
-		bool contains(T element) const;
+			delete[] data_;
+			data_ = tmp;
+			length_++;
+		}
 
-		T *to_array() const;
+		void remove(const T element)
+		{
+			if (!contains(element))
+				return;
+
+			T* tmp = new T[length_ - 1];
+			for (int i = 0, t = 0; i < length_; i++)
+			{
+				if (data_[i] != element)
+				{
+					tmp[t] = data_[i];
+					t++;
+				}
+			}
+
+			delete[] data_;
+			data_ = tmp;
+			length_--;
+		}
+
+		bool contains(T element) const
+		{
+			for (int i = 0; i < length_; i++)
+			if (data_[i] == element)
+				return true;
+			return false;
+		}
+
+		T *to_array() const
+		{
+			T *arr = new T[length_];
+			for (int i = 0; i < length_; i++)
+			{
+				arr[i] = data_[i];
+			}
+			return arr;
+		}
+
+		Set<int> clone()
+		{
+			return Set<int>(*this);
+		}
+
+		
+		std::string to_string() const
+		{			
+			std::string res = "";
+			if (length_ == 0)
+			{
+				res += "(empty set)";
+				return res;
+			}
+			for (int i = 0; i < length_; i++)
+			{
+				res += std::to_string(data_[i]);
+				if (i < length_ - 1)
+					res += " ";
+			}
+			return res;
+		}
 	};
 }
---
+++ OOP_Labs/Lab_02/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <conio.h>
+#include "Set.h"
+
+using namespace Util;
+using namespace std;
+
+int main()
+{
+	Set<int> set = Set<int>();
+
+	// Test 1: adding an element, which is contained
+	set += 0;
+	set += 1;
+	set += 2;
+	set += 3;
+	set += 2;
+	cout << set.to_string() << endl;
+
+	// Test 2: Removing elements
+	set -= 0;
+	set -= 3;
+	cout << set.to_string() << endl;
+
+	// Test 3a: Union. +=
+	int *arr_a = new int[3]{ 8, 7, 6 };
+	Set<int> other_a = Set<int>(arr_a, 3);
+	set += other_a;
+	delete[] arr_a;
+	cout << set.to_string() << endl;
+
+	// Test 3b: Union. +
+	int *arr_b = new int[4]{ 1, 2, 6, 9 };
+	Set<int> other = Set<int>(arr_b, 4);
+
+	Set<int> united = set + other;
+	delete[] arr_b;
+
+	cout << set.to_string() << 
+		" + " << 
+		other.to_string() <<
+		" = " <<
+		united.to_string() <<
+		endl;
+
+
+	// Test 4a. Intersection. *=
+	int *arr4_a = new int[4]{ 1, 2, 3, 7 };
+	Set<int> other4_a = Set<int>(arr4_a, 4);
+	cout << set.to_string() << " *= " << other4_a.to_string() << " == ";
+	set *= other4_a;
+	delete[] arr4_a;
+	cout << set.to_string() << endl;
+
+	// Test 4b. Intersection. *
+	set += 4;
+	set += 5;
+	set += 10;
+	int *arr4_b = new int[4]{ 1, 10, 3, 7 };
+	Set<int> other4_b = Set<int>(arr4_b, 4);
+	cout << set.to_string() << " * " << other4_b.to_string() << " = ";
+	Set<int> intersetion = set * other4_b;
+	delete[] arr4_b;
+	cout << intersetion.to_string() << endl;
+
+	// Test 5a. Bisection. -=
+	int *arr5_a = new int[4]{ 1, 2, 3, 7 };
+	Set<int> other5_a = Set<int>(arr5_a, 4);
+	cout << set.to_string() << " -= " << other5_a.to_string() << " == ";
+	set -= other5_a;
+	delete[] arr5_a;
+	cout << set.to_string() << endl;
+
+	// Test 5b. Bisection. -
+	set += 1;
+	set += 2;
+	set += 3;
+	int *arr5_b = new int[4]{ 1, 10, 3, 7 };
+	Set<int> other5_b = Set<int>(arr5_b, 4);
+	cout << set.to_string() << " - " << other4_b.to_string() << " = ";
+	Set<int> bis = set - other4_b;
+	delete[] arr5_b;
+	cout << bis.to_string() << endl;
+
+	// Test 6a. Comparing different
+	bool equal_a = set == other;
+	cout << set.to_string() <<
+		" == " <<
+		other.to_string() <<
+		" = " <<
+		equal_a <<
+		endl;
+
+	// Test 6b. Comparing equal
+	Set<int> same = set.clone();
+	bool equal_b = set == same;
+	cout << set.to_string() <<
+		" == " <<
+		same.to_string() <<
+		" = " <<
+		equal_b <<
+		endl;
+
+	// Test 6c. Comparing empty
+	Set<int> empty1 = Set<int>();
+	Set<int> empty2 = Set<int>();
+	bool equal_c = empty1 == empty2;
+	cout << empty1.to_string() <<
+		" == " <<
+		empty2.to_string() <<
+		" = " <<
+		equal_c <<
+		endl;
+
+	_getch();
+}
